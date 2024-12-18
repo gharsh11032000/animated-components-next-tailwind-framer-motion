eaed4003bfd17a8b2881465a6bcc68b1e55eaf5d
gharsh11032000 <72333300+gharsh11032000@users.noreply.github.com>
2024-12-19 00:06:25 +0530

    Updating Text Animation 7

--- app/ui/text-animation-7/page.tsx
+++ app/ui/text-animation-7/page.tsx
@@ -18,8 +18,26 @@ export default function Section() {
         transition={{
           duration: 1.5,
         }}
-        className="min-h-screen grid place-content-center"
+        className="min-h-screen grid place-content-center relative overflow-hidden"
       >
+        <motion.div
+          animate={{
+            opacity: [0.8, 1],
+            scale: [1.8, 2],
+            rotate: [0, 360, 0],
+          }}
+          transition={{
+            duration: 4,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+          style={{
+            left: "calc(50% - 12rem)",
+          }}
+          className="absolute -top-1/4 z-10 w-96 h-96 bg-gradient-to-r from-blue-400 to-purple-500 rounded-full blur-[8rem] opacity-100"
+        ></motion.div>
+
         <h1 className="text-4xl md:text-5xl gap-4 lg:gap-6 flex flex-col md:flex-row items-center lg:text-7xl font-bold text-white">
           <span>A cloning tool for</span>
           <div className="lg:h-32 px-4 md:px-6 h-16 md:h-20 rounded-2xl lg:rounded-3xl overflow-hidden border border-blue-400">
