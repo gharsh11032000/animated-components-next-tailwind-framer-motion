bd9da87965be4908429f8cee28d1cefad8518af0
gharsh11032000 <72333300+gharsh11032000@users.noreply.github.com>
2024-12-22 20:26:29 +0530

    Adding Text Animation 14

---
+++ app/ui/text-animation-14/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Section() {
+  return (
+    <div className="relative overflow-x-hidden dark:bg-gray-950">
+      <motion.section
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="min-h-screen w-full grid place-content-center relative overflow-hidden"
+      >
+        <motion.div
+          animate={{
+            opacity: [0.8, 1],
+            rotate: [0, 360, 0],
+          }}
+          transition={{
+            duration: 8,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+          style={{
+            left: "calc(50% - 12rem)",
+            bottom: "-12rem",
+            scale: 2,
+          }}
+          className="absolute w-96 h-96 bg-gradient-to-r from-yellow-500 to-red-400 rounded-full blur-3xl opacity-100"
+        ></motion.div>
+
+        <div className="uppercase font-bold w-full text-5xl md:text-6xl lg:text-8xl text-gray-100">
+          <AnimatedWord word={"illumination"} />
+        </div>
+
+        <div className="m-4 text-xl text-gray-300 text-center">
+          <p>The process of lighting up a space.</p>
+        </div>
+      </motion.section>
+    </div>
+  );
+}
+
+interface AnimatedWordProps {
+  word: string;
+  interval?: number;
+  className?: string;
+}
+
+function AnimatedWord({ word, className }: AnimatedWordProps) {
+  return (
+    <div
+      style={{
+        perspective: "1000px",
+      }}
+      className={`inline-flex ${className}`}
+    >
+      <>
+        {word.split("").map((letter, index) => {
+          const centerIndex = (word.length - 1) / 2;
+          const z = Math.abs(centerIndex - index) * 40 + 360;
+          const delay = Math.abs(centerIndex - index) * 0.1;
+
+          return (
+            <motion.div
+              style={{
+                transformStyle: "preserve-3d",
+              }}
+              initial={{
+                opacity: 0,
+                z: z,
+                filter: "blur(12px)",
+                rotateY: 180,
+              }}
+              animate={{
+                opacity: 1,
+                z: 0,
+                filter: "blur(0px)",
+                rotateY: 0,
+              }}
+              transition={{
+                delay,
+                duration: 0.75,
+                ease: "easeOut",
+              }}
+              key={index}
+            >
+              {letter}
+            </motion.div>
+          );
+        })}
+      </>
+    </div>
+  );
+}
