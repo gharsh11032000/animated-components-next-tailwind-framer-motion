5c3f0f657ed21852c5e8a716422788e9abd39ab1
gharsh11032000 <gharsh11032000@gmail.com>
2024-12-23 17:00:47 +0530

    Adding Text Animation 15

---
+++ app/ui/text-animation-15/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Section() {
+  return (
+    <div className="relative overflow-x-hidden dark:bg-gray-950">
+      <motion.section
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="min-h-screen w-full grid place-content-center relative overflow-hidden"
+      >
+        <motion.div
+          animate={{
+            opacity: [0.8, 1],
+            rotate: [0, 360, 0],
+          }}
+          transition={{
+            duration: 8,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+          style={{
+            left: "calc(50% - 12rem)",
+            bottom: "-12rem",
+            scale: 2,
+          }}
+          className="absolute w-96 h-96 bg-gradient-to-r from-gray-400 to-gray-300 rounded-full blur-3xl opacity-100"
+        ></motion.div>
+
+        <div className="font-bold w-full text-5xl md:text-6xl lg:text-8xl text-gray-100">
+          <AnimatedWord word={"Gravity"} className="w-full" />
+        </div>
+
+        <div className="m-4 text-xl text-gray-300 text-center">
+          <p>The process of lighting up a space.</p>
+        </div>
+      </motion.section>
+    </div>
+  );
+}
+
+interface AnimatedWordProps {
+  word: string;
+  interval?: number;
+  className?: string;
+}
+
+function AnimatedWord({ word, className }: AnimatedWordProps) {
+  const colors = ["text-red-500", "text-green-500", "text-blue-500", "text-pink-500", "text-white"];
+
+  return (
+    <div
+      style={{
+        perspective: "1000px",
+      }}
+      className={`inline-flex ${className}`}
+    >
+      <span className="text-transparent invisible opacity-0 pointer-events-none">
+        {word}
+      </span>
+
+      {
+        new Array(5).fill(word).map((_, parentIndex) => {
+          return (
+            <motion.div
+              style={{
+                transformStyle: "preserve-3d",
+              }}
+              className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex"
+              key={parentIndex}
+            >
+
+              {word.split("").map((letter, index) => {
+                const delay = index * 0.075 + parentIndex * 0.075;
+                return (
+                  <motion.div
+                    style={{
+                      transformStyle: "preserve-3d",
+                    }}
+                    className={colors[parentIndex % colors.length]}
+                    initial={{
+                      opacity: 0,
+                      z: 360,
+                    }}
+                    animate={{
+                      opacity: 1,
+                      z: 0,
+                    }}
+                    transition={{
+                      delay,
+                      duration: 0.5,
+                      ease: "easeOut",
+                    }}
+                    key={index}
+                  >
+                    {letter}
+                  </motion.div>
+                );
+              })}
+            </motion.div>
+          );
+        })
+      }
+    </div>
+  );
+}
